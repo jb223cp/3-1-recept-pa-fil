a6c65bf2bee461b0deaf06c7ac2903c7df7a2028
Jasmin Bejtovic <jb223cp@student.lnu.se>
2014-10-22 13:34:58 +0200

    Klar. Save() har lite funktionalitet för att testa inskrivning

--- FiledRecipes/FiledRecipes.Domain/RecipeRepository.cs
+++ FiledRecipes/FiledRecipes.Domain/RecipeRepository.cs
@@ -127,5 +127,172 @@ namespace FiledRecipes.Domain
                 handler(this, e);
             }
         }
+        /// <summary>
+        /// Read recipes from text file
+        /// </summary>
+        public virtual void Load()
+        {
+            List<IRecipe> listOfRecipes = new List<IRecipe>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(_path))
+                {
+                    RecipeReadStatus status = RecipeReadStatus.Indefinite;
+                    string line = null;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+
+                        switch (line)
+                        {
+                            case "":
+                                continue;
+                            case SectionRecipe:
+                                status = RecipeReadStatus.New;
+                                break;
+                            case SectionIngredients:
+                                status = RecipeReadStatus.Ingredient;
+                                break;
+                            case SectionInstructions:
+                                status = RecipeReadStatus.Instruction;
+                                break;
+                            default:
+                                switch (status)
+                                {
+                                    case RecipeReadStatus.New:
+                                        listOfRecipes.Add(new Recipe(line));
+                                        continue;
+                                    case RecipeReadStatus.Ingredient:
+                                        string[] ingredientsParts = line.Split(';');
+                                        if (ingredientsParts.Length != 3)
+                                            throw new FileFormatException();
+
+                                        Ingredient ingredient = new Ingredient();
+                                        ingredient.Amount = ingredientsParts[0];
+                                        ingredient.Measure = ingredientsParts[1];
+                                        ingredient.Name = ingredientsParts[2];
+
+                                        listOfRecipes.Last().Add(ingredient);
+                                        break;
+                                    case RecipeReadStatus.Instruction:
+                                        listOfRecipes.Last().Add(line);
+                                        break;
+                                    default:
+                                        throw new FileFormatException();
+                                }
+                                break;
+                        }
+                    }
+                    _recipes = listOfRecipes.OrderBy(r => r.Name).ToList();
+                    IsModified = false;
+                    OnRecipesChanged(EventArgs.Empty);
+                }
+            }
+            catch (FileFormatException ex)
+            {
+                Console.WriteLine("Ett oväntat fel inträffade.\n{0}",
+           ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ett oväntat fel inträffade.\n{0}",
+                ex.Message);
+            }
+        }
+        /// <summary>
+        /// Method that writes new recipe into text file.
+        /// Method implements functionality to save new recipe from console.
+        /// </summary>
+        public virtual void Save()
+        {
+            try
+            {
+                //Initialize StreamWriter object and write strings into text file
+
+                using (StreamWriter writer = new StreamWriter(_path,true))
+                {
+                    writer.WriteLine(RecipeReadStatus.New);
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("\nAnge receptens namn: ");
+                    Console.ResetColor();
+                    writer.WriteLine(Console.ReadLine());
+                    writer.WriteLine(RecipeReadStatus.Ingredient);
+
+                    List<string> ingredienser = new List<string>();
+                    string ingrediens = "";
+                    string userInput = "";
+                    do
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("\nAnge ingrediensens mängd: ");
+                        Console.ResetColor();
+                        ingrediens = Console.ReadLine() + ";";
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("\nAnge ingrediensens mått: ");
+                        Console.ResetColor();
+                        ingrediens += (Console.ReadLine()) + ";";
+
+                        /* Checks if ingrediensens name is empty, and iterate through until it´s not empty*/
+                        do
+                        {
+                            /* User friendly test */
+                            Console.BackgroundColor = ConsoleColor.DarkBlue;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write("\nAnge ingrediensens namn:");
+                            Console.ResetColor();
+                            /* end */
+
+                            userInput = Console.ReadLine();
+                            if (!String.IsNullOrEmpty(userInput))
+                                ingrediens += userInput;
+                        } while (String.IsNullOrEmpty(userInput));
+
+                        ingredienser.Add(ingrediens);
+
+                        /* User friendly test */
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("\nTryck tangent för att fortsätta med ingredienser - [Esc Instruktioner]:");
+                        Console.ResetColor();
+                        /* end */
+
+                    } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+                    foreach (String item in ingredienser)
+                    {
+                        writer.WriteLine(item);
+                    }
+                    writer.WriteLine(RecipeReadStatus.Instruction);
+                    userInput = "";
+                    do
+                    {
+                        /* User friendly test */
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("\n\nSkriv en ny instruktion:");
+                        Console.ResetColor();
+                        /* end */
+                        userInput = Console.ReadLine();
+                        writer.WriteLine(userInput);
+
+                        /* User friendly test */
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("\nTryck tangent för att fortsätta med instruktioner - Esc, Spara ny recept]:");
+                        Console.ResetColor();
+                        /* end */
+
+                    } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+                    IsModified = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ett oväntat fel inträffade.\n{0}",
+                ex.Message);
+            }
+        }
     }
 }
--- FiledRecipes/FiledRecipes/Views/RecipeView.cs
+++ FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -4,6 +4,7 @@ using FiledRecipes.Properties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace FiledRecipes.Views
 {
@@ -12,5 +13,37 @@ namespace FiledRecipes.Views
     /// </summary>
     public class RecipeView : ViewBase, IRecipeView
     {
+        public void Show(IRecipe recipe)
+        {
+            Console.Clear();
+            ShowPanel(recipe.Name,ConsoleColor.White,ConsoleColor.DarkCyan);
+            Console.WriteLine();
+            Console.WriteLine("Ingredienser");
+            Console.WriteLine("============");
+            foreach (Ingredient item in recipe.Ingredients)
+            {
+                   Console.WriteLine("{0} {1} {2}",item.Amount ,item.Measure, item.Name);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Gör så här");
+            Console.WriteLine("==========");
+
+            int i = 1;
+            foreach(string item in recipe.Instructions)
+            {
+                Console.WriteLine("<{0}>", i);
+                Console.WriteLine(item);
+                i++;
+            }
+        }
+
+        public void Show(IEnumerable<IRecipe> recipes)
+        {
+            foreach (IRecipe item in recipes)
+            {
+                Show(item);
+                ContinueOnKeyPressed();
+            }
+        }
     }
 }
